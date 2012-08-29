50bf2cbcd4c2810756ffb3c84d30de7e0560c5a1
xitsalarauko <xitsalarauko@d39da7f4-2e98-11de-bfb9-e98281094dd5>
2012-08-29 21:18:17 +0000

    Added options dialog to choose between different strategies for file disposition.

--- resource.h
+++ resource.h
@@ -8,6 +8,7 @@
 #define IDS_ABOUTBOX                    101
 #define IDD_HARDLINK_DIALOG             102
 #define IDR_MAINFRAME                   128
+#define IDD_SPLIT_OPTIONS               129
 #define IDC_COPY_FROM_BROWSE            1001
 #define IDC_COPY_FROM                   1002
 #define IDC_COPY_TO                     1003
@@ -16,21 +17,27 @@
 #define IDC_SPLITSIZE                   1006
 #define IDC_EDIT2                       1007
 #define IDC_LOG                         1007
+#define IDC_LOGICAL_STRUCTURE           1008
+#define IDC_MAXIMUM_PACKING             1009
+#define IDC_OS_STOP                     1010
 #define IDC_SPLIT_FROM                  1011
+#define IDC_OS_WARN                     1011
 #define IDC_SPLIT_FROM_BROWSE           1012
+#define IDC_OS_OVERSIZED                1012
 #define IDC_SPLIT_TO                    1013
 #define IDC_SPLIT_TO_BROWSE             1014
 #define IDC_DOSPLIT                     1015
 #define IDC_DOCLEAN_COPY                1016
 #define IDC_DOCLEAN_SPLIT               1017
+#define IDC_BTN_OPTIONS                 1018
 
 // Next default values for new objects
-// 
+//
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_RESOURCE_VALUE        130
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1008
+#define _APS_NEXT_CONTROL_VALUE         1019
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
